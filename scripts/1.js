ca05b82c502b297059f25f555be3b0149fa3a61c
Oleg <olegandrishko2000@gmail.com>
2023-01-07 15:37:04 +0100

    modified:   .gitignore 	new file:   index.html 	new file:   scripts/1.js

---
+++ scripts/1.js
@@ -0,0 +1,81 @@
+// 1
+
+const str = 'Привіт';
+const num = 123;
+const flag = true;
+const txt = 'true';
+
+console.log(`Змінна str: ${typeof str}`);
+console.log(`Змінна num: ${typeof num}`);
+console.log(`Змінна flag: ${typeof flag}`);
+console.log(`Змінна txt: ${typeof txt}`);
+
+// 2
+
+const Pi = 3.14;
+const name = 'Oleg';
+const password = 'qwerty123';
+const homeworkNumber = 1;
+const userAnswer = true;
+let age = 14;
+let type = 'string';
+let isHere = false;
+let definition = undefined;
+let number = NaN;
+
+// 3
+
+const userName = prompt('Ім’я користувача?');
+console.log(`Ім’я користувача: ${userName}`);
+
+// 4
+
+let myAge = 14;
+let myFriendsName = 15;
+alert(myAge + myFriendsName);
+
+// 5
+let a = 4;
+let b = 67;
+console.log(a * b);
+
+// 6
+
+let a1 = 5 + 3;
+let a2 = 5 - 3;
+let a3 = 5 * 3;
+let a4 = 5 / 3;
+let a5 = 5 % 3;
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);
+
+// 7
+
+let a6 = 5 % 3;
+let a7 = 3 % 5;
+let a8 = 5 + '3';
+let a9 = '5' - 3;
+let a10 = 75 + 'кг';
+
+alert(a6);
+alert(a7);
+alert(a8);
+alert(a9);
+alert(a10);
+
+// 8
+
+let height = '23';
+let width = '10';
+let S = `Площа прямокутника: ${height * width}см`;
+console.log(S);
+
+// 9
+
+let heightC = 10;
+let dC = 4;
+let v = `Oбсяг циліндра: ${heightC * dC * Pi}м`;
+console.log(v);
