e28eb88716b9562beea12fd1754db158da7257bf
Oleg <olegandrishko2000@gmail.com>
2023-02-03 18:53:51 +0100

    modified:   index.html 	modified:   scripts/4.js

---
+++ scripts/4.js
@@ -0,0 +1,30 @@
+// 1
+
+for (let i = 1; i < 11; i++) {
+    console.log(i);
+}
+
+// 2
+
+for (i = 1; i < 11; i++) {
+    if (i % 2 == 0) {
+        console.log(`${i} - Число парне`);
+    } else {
+        console.log(`${i} - Число непарне`)
+    }
+}
+
+// 3
+
+let num = 20;
+do {
+    num++;
+    console.log(num)
+} while (num <= 28);
+
+// 4
+
+let hName = prompt("Hero:");
+if (hName.length < 6) {
+    console.log(hName);
+}
